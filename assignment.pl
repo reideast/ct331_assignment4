b5074cbc96de0d3d2b54cb085dc2db24e348327b
Andrew Reid East <andrew@andreweast.net>
2017-11-09 15:08:21 +0000

    Question 1 - is element in list?

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,13 @@
 %Definitions for isElementInList(El, List)
+% Base Case: El is the head of List
+isElementInList(El, [El | _]).
+% Recursive Case: List is reduced to everything except the head of the list
+% I.e. Element is in the list if element is in the tail of the list...somewhere!
+isElementInList(El, [_ | Tail]) :- isElementInList(El, Tail).
 
 %Definitions for reverseList(List, ReversedList)
 
 %insertElementIntoListEnd(El, List, NewList)
 
 %Definitions for mergeLists(List1, List2, Merged)
+
