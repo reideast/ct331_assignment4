2b77f265be448709b90c2db40f7c3fa3c30eb182
Andrew Reid East <andrew@andreweast.net>
2017-11-12 13:13:08 +0000

    Clean up code Q2

--- assignment.pl
+++ assignment.pl
@@ -1,34 +1,25 @@
 %Definitions for isElementInList(El, List)
+
 % Base Case: El is the head of List
 isElementInList(El, [El | _]).
+% Other Base Case: List is reduced to the empty list. This should be false. In Prolog, nonexistance == false
+
 % Recursive Case: List is reduced to everything except the head of the list
 % I.e. Element is in the list if element is in the tail of the list...somewhere!
-isElementInList(El, [_ | Tail]) :- isElementInList(El, Tail).
+isElementInList(El, [_ | Tail]) :-
+    isElementInList(El, Tail).
+
+
 
 %Definitions for mergeLists(List1, List2, Merged)
+
 % Base Case: First list is now mepty. Second list is the basis of the new, merged list
 mergeLists([], List2, List2).
-% Recursive Case: First list still has elements. Don't care what the second list is, but
-mergeLists([Head | Tail], List2, [Head | Merged]) :- mergeLists(Tail, List2, Merged).
 
-
-% Base Case: Both lists are empty, and Merged is already a list
-%mergeLists([], [], Merged) :- Merged = [_ | _].
-%% Recursive Case: First list still has elements, but second list is empty
-%mergeLists([Head | Tail], [], SmallerList) :- mergeLists(Tail, [], [Head | SmallerList]).
-%% Recursive Case: Second list still has elements, but first list is empty
-%mergeLists([], [Head | Tail], SmallerList) :- mergeLists([], Tail, [Head | SmallerList]).
-%% Recursive Case: Neither list is empty?!???
-
-
-%% Base case: Both inputs are empty
-%mergeLists([], [], ).
-%% Recursive case: First input is empty, but second input still has elements
-%mergeLists([], [Head | Tail], NewList) :- mergeLists([], Tail, [Head | NewList]).
-%% Recursive case: First input still has elements
-%mergeLists([Head | Tail], List2, NewList) :- mergeLists(Tail, List2, [Head | NewList]).
-
-%mergeLists(List1, List2, Merged) :- Merged = [List1 | List2].
+% Recursive Case: First list still has elements. It's head (atom) onto whatever the reduced call returns as Merged will become the new merged list
+% Don't care what the second list is, but pass it on down towards the base case (the string)
+mergeLists([Head | Tail], List2, [Head | Merged]) :-
+    mergeLists(Tail, List2, Merged).
 
 
 %insertElementIntoListEnd(El, List, NewList)
